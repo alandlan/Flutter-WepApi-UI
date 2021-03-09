08d108ac8e102e1b6d1e64368a01a5913794c3df
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-03-09 13:47:31 -0300

    Recebendo valor do AlertDialog

--- lib/components/transaction_auth_dialog.dart
+++ lib/components/transaction_auth_dialog.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TransactionAuthDialog extends StatelessWidget {
+class TransactionAuthDialog extends StatefulWidget {
+  final Function(String password) onConfirm;
+
+  TransactionAuthDialog({@required this.onConfirm});
+
+  @override
+  _TransactionAuthDialogState createState() => _TransactionAuthDialogState();
+}
+
+class _TransactionAuthDialogState extends State<TransactionAuthDialog> {
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Authenticate'),
       backgroundColor: Colors.grey.shade300,
       content: TextField(
+        controller: _passwordController,
         obscureText: true,
         maxLength: 4,
         decoration: InputDecoration(
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16.0))
-          ),
+              borderRadius: BorderRadius.all(Radius.circular(16.0))),
         ),
-
-        style: TextStyle(fontSize: 64,letterSpacing: 16),
+        style: TextStyle(fontSize: 64, letterSpacing: 16),
         keyboardType: TextInputType.number,
         textAlign: TextAlign.center,
       ),
       actions: <Widget>[
         FlatButton(
-          onPressed: (){ print('cancel');},
+          onPressed: () {
+            Navigator.pop(context);
+          },
           child: Text('Cancel'),
-        ),FlatButton(
-          onPressed: (){ print('confirm');},
+        ),
+        FlatButton(
+          onPressed: () {
+            widget.onConfirm(_passwordController.text);
+            Navigator.pop(context);
+          },
           child: Text('Confirm'),
         )
       ],
